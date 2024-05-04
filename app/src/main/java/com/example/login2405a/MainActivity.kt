d257001aeca99e271feda9bede26dd45e4c27397
habilithm97 <habinkim97@gmail.com>
2024-05-04 23:29:34 +0900

    로그인 UI

--- app/src/main/java/com/example/login2405a/MainActivity.kt
+++ app/src/main/java/com/example/login2405a/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.login2405a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.login2405a.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+
     }
 }
